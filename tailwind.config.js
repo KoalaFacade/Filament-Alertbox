b5d54b9a7ea0aaa3298cba7bf8f7b25b87f13087
pemudakoding <pemudakoding@gmail.com>
2023-03-10 22:10:44 +0800

    build: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./resources/views/**/*.blade.php', './src/**/*.php'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

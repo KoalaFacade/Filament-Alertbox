82f7938ce624f3c62fd516440284c539162f9502
pemudakoding <pemudakoding@gmail.com>
2023-03-10 22:22:12 +0800

    fix: missing tailwind classes

--- src/FilamentAlertBoxServiceProvider.php
+++ src/FilamentAlertBoxServiceProvider.php
@@ -2,11 +2,18 @@
 
 namespace KoalaFacade\FilamentAlertBox;
 
+use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentAlertBoxServiceProvider extends PackageServiceProvider
+class FilamentAlertBoxServiceProvider extends PluginServiceProvider
 {
+    /**
+     * @var string[]
+     */
+    protected array $styles = [
+        'plugin-filament-alertbox-styles' => __DIR__ . '/../resources/dist/filament-alertbox.css',
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./resources/views/**/*.blade.php', './src/**/*.php'],
   theme: {
